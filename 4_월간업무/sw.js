b586ea05e7fa36951f22e21d5cd37ca4487ea3d5
junshik6587-droid <junshik6587@gmail.com>
2026-07-30 17:02:16 +0900

    Add files via upload

---
+++ 4_월간업무/sw.js
@@ -0,0 +1,39 @@
+/* 현장 안전관리 서식 - Service Worker */
+const CACHE = 'safety-forms-v3';
+const SHELL = [
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(SHELL)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+/* 네트워크 우선, 실패 시 캐시. 방문한 서식 페이지는 캐시에 저장해 오프라인 지원 */
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then((r) => r || caches.match('./index.html')))
+  );
+});
